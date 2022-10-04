954c010ffea8ee61d16b13c3744035657318bd8e
Adam Smshr <itseedris@gmail.com>
2022-10-04 10:22:11 +0100

    Upload

---
+++ spec/my-package-testing--view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MyPackageTesting-View from '../lib/my-package-testing--view';
+
+describe('MyPackageTesting-View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
